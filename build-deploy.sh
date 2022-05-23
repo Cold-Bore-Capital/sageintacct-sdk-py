7322eda9641051cdf3142eff547fd9e98440b60d
adhamsuliman <adhamsuliman93@gmail.com>
2022-05-23 12:55:16 -0400

    updating to 0.0.11

--- build-deploy.sh
+++ build-deploy.sh
@@ -2,4 +2,5 @@
 
 python setup.py sdist
 twine upload dist/*
+#twine upload --skip-existing dist/*
 rm dist/*
--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='cbc-sageintacctsdk',
-    packages=['cbc-sageintacctsdk']
+    name='cbcintacctsdk',
+    packages=['cbcintacctsdk'],
+    version='0.0.11',
+    # package_dir={'': '*'},
+    # install_requires=['*']
 )
